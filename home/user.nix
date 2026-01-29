5ecabd83b6fbba70f4fc046ed91880ceddb4e4df
Fluffy Pal <fluffypal17@gmail.com>
2026-01-30 00:16:07 +0300

    style: finalize user config with bat, lolcat and custom prompt

--- home/user.nix
+++ home/user.nix
@@ -6,8 +6,35 @@
   home.stateVersion = "25.11"; 
 
   home.packages = with pkgs; [
+    helix
+    lolcat
+    bat
   ];
 
+  programs.bash = {
+      enable = true;
+      enableCompletion = true;
+      historySize = 1000;
+      historyControl = [ "ignoredups" "erasedups" ];
+      shellAliases = {
+        ll = "ls -lh";
+        la = "ls -lha";
+        cat = "lolcat";
+        hx = "helix"; 
+        helix = "helix";
+      };
+  
+      bashrcExtra = ''
+        # Sadece Nix'in otomatik yapamadığı "Görsel" ve "Dış Kaynaklı" ayarlar:
+        
+        # Renkli Prompt (Kişisel zevk)
+        PS1="\[\e[38;2;255;171;185m\]\u@\h \[\e[38;2;180;200;255m\]\w\[\e[0m\]\$ "
+  
+        # Eğer bu dosya GitLab'da yoksa hata almamak için kontrol ekledik
+        [ -f "$HOME/.alias" ] && source "$HOME/.alias"
+      '';
+    };
+    
   services.flatpak = {
       enable = true;
       packages = [
