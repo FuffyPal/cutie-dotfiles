8229bd6ebed5a06c8e832dcee84617d96a23c837
Fluffy Pal <fluffypal17@gmail.com>
2026-01-30 03:26:38 +0300

    feat: enable auto-upgrade and system maintenance (GC & limits)

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -8,8 +8,26 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    nix.settings.auto-optimise-store = true; 
+    
+    nix.gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+  system.autoUpgrade = {
+    enable = true;
+    flake = "github:FuffyPal/cutie-dotfiles";
+    flags = [
+      "--update-input" "nixpkgs"
+      "--commit-lock-file"
+    ];
+    dates = "04:00";
+    randomizedDelaySec = "45min";
+  };
   
   services.flatpak.enable = true;
 
