da6fde99eb2a777587869308278145b8cf9eebd7
Fluffy Pal <fluffypal17@gmail.com>
2026-01-30 21:33:17 +0300

    feat: enable Podman containers and update the container registry search list.

--- hosts/nixos/podman.nix
+++ hosts/nixos/podman.nix
@@ -8,10 +8,12 @@
     defaultNetwork.settings.dns_enabled = true;
   };
 
-  virtualisation.containers.registries.search = [ 
-    "docker.io"
-    "ghcr.io"
-    "quay.io"
-    "registry.gitlab.com"
-  ];
+virtualisation.containers = {
+    enable = true;
+    registries.search = [
+      "docker.io"
+      "ghcr.io"
+      "quay.io"
+    ];
+  };
 }
