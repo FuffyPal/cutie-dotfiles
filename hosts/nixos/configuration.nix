c71141979f4dfab6958ab77ee850d3a8c5361fad
Fluffy Pal <fluffypal17@gmail.com>
2026-01-30 22:50:32 +0300

    feat: add subUid and subGid ranges to user configuration

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -96,6 +96,8 @@
       "tailscale"
       "docker"
     ];
+    subUidRanges = [{ startUid = 100000; count = 65536; }];
+    subGidRanges = [{ startGid = 100000; count = 65536; }];
   };
 
   system.stateVersion = "25.11";
