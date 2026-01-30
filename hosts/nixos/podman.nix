39e25978435a7082b7b17440c347b00231f448ad
Fluffy Pal <fluffypal17@gmail.com>
2026-01-30 21:31:17 +0300

    feat: Enable Podman's Docker socket and expand container registry search to include ghcr.io, quay.io, and registry.gitlab.com.

--- hosts/nixos/podman.nix
+++ hosts/nixos/podman.nix
@@ -4,8 +4,14 @@
   virtualisation.podman = {
     enable = true;
     dockerCompat = true;
+    dockerSocket.enable = true;
     defaultNetwork.settings.dns_enabled = true;
   };
 
-  virtualisation.containers.registries.search = [ "docker.io" ];
+  virtualisation.containers.registries.search = [ 
+    "docker.io"
+    "ghcr.io"
+    "quay.io"
+    "registry.gitlab.com"
+  ];
 }
