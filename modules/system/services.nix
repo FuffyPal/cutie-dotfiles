b8d5057e1bb3a82cdced6a00fd76495d0a1abd2f
Fluffy Pal <fluffypal17@gmail.com>
2026-01-30 15:05:21 +0300

    Update services.nix

--- modules/system/services.nix
+++ modules/system/services.nix
@@ -4,31 +4,37 @@
   services.syncthing = {
     enable = true;
     user = "${userSettings.username}";
-    dataDir = "/home/${userSettings.username}/Documents";    
-    configDir = "/home/${userSettings.username}/.config/syncthing"; 
+    dataDir = "/home/${userSettings.username}/Documents";
+    configDir = "/home/${userSettings.username}/.config/syncthing";
     settings.devices."pal".id = "${systemSettings.syncthingId}";
     settings.folders = {
       "Music" = {
+        id = "2b9rx-xkurd";
         path = "/home/${userSettings.username}/Music";
         devices = [ "pal" ];
       };
       "Videos" = {
+        id = "pgdd4-ddwkm";
         path = "/home/${userSettings.username}/Videos";
         devices = [ "pal" ];
       };
       "Pictures" = {
+        id = "h5vho-ht2o2";
         path = "/home/${userSettings.username}/Pictures";
         devices = [ "pal" ];
       };
       "Documents" = {
+        id = "s2q4h-pqhiz";
         path = "/home/${userSettings.username}/Documents";
         devices = [ "pal" ];
-      }; 
+      };
       "Joplin" = {
+
         path = "/home/${userSettings.username}/Sync/joplin";
         devices = [ "pal" ];
       };
       "Sec" = {
+        id = "a74cx-jd4jh";
         path = "/home/${userSettings.username}/Sync/sec";
         devices = [ "pal" ];
       };
