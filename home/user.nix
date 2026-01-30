9c3030fdfc9c1851137f956b0be3b512ec857d45
Fluffy Pal <fluffypal17@gmail.com>
2026-01-30 21:43:45 +0300

    feat: Add container registry configuration for Docker, GHCR, and Quay.

--- home/user.nix
+++ home/user.nix
@@ -51,6 +51,14 @@ with lib.hm.gvariant;
     gotools
   ];
 
+  xdg.configFile."containers/registries.conf".text = ''
+    [registries.search]
+    registries = ['docker.io', 'ghcr.io', 'quay.io']
+
+    [registries.short-name-mode]
+    mode = "permissive"
+  '';
+
   programs.bash = {
     enable = true;
     enableCompletion = true;
