6a7d04d96cc1394d67832edc9f6f8397647f7777
Fluffy Pal <fluffypal17@gmail.com>
2026-02-03 18:33:09 +0300

    chore: remove Hytale flatpak installation from home activation.

--- home/user.nix
+++ home/user.nix
@@ -52,12 +52,6 @@ with lib.hm.gvariant;
     golangci-lint
     gotools
   ];
-
-  home.activation = {
-    installHytale = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      $DRY_RUN_CMD ${pkgs.flatpak}/bin/flatpak install --user -y --noninteractive https://launcher.hytale.com/builds/release/linux/amd64/hytale-launcher-latest.flatpak || true
-    '';
-  };
   
   programs.bash = {
     enable = true;
