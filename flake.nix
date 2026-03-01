f6a47253b62bd8adb7edde783618362cc0b743fb
Fluffy Pal <fluffypal17@gmail.com>
2026-03-02 00:29:50 +0300

    chore: remove syncthing service configuration and related directories from system services

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
         # please enter sudo lshw -c display PCI area or lspci | grep -E 'VGA|3D'
         amdgpuBusId = "PCI:5:0:0";
         nvidiaBusId = "PCI:1:0:0";
-        syncthingId = "None";
+        syncthingId = "None"; # dont working because i dont have syncthing installed
       };
       userSettings = {
         username = "fluffypal";
