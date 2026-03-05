4af3088a9f244f3b874d21d457a28eeae067dbef
Fluffy Pal <fluffypal17@gmail.com>
2026-03-05 21:19:54 +0300

    chore: update ananicy service settings to enhance performance and logging

--- modules/system/ananicy.nix
+++ modules/system/ananicy.nix
@@ -6,6 +6,13 @@
     package = pkgs.ananicy-cpp;
     rulesProvider = pkgs.ananicy-rules-cachyos;
     settings = {
+      apply_nice = true;
+      apply_latencies = true;
+      apply_sched = true;
+      apply_io = true;
+      apply_cgroup = false; 
+      
+      log_level = "warn";
     };
     extraRules = [
     ];
