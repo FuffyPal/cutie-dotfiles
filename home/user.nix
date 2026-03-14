7308486567018527b100ba04dcb51326ce1b0a31
Fluffy Pal <email@email.com>
2026-03-14 17:38:57 +0300

    Update user configuration: Adjusted indicator position, modified Bluetooth device list timestamps, refined caffeine timer settings, and added new layout and snap assistant threshold for tilingshell.

--- home/user.nix
+++ home/user.nix
@@ -215,21 +215,16 @@ with lib.hm.gvariant;
       charging-mode = "max";
       device-type = 12;
       icon-style-type = 0;
-      indicator-position-max = 5;
+      indicator-position-max = 3;
       polkit-status = "installed";
       show-battery-panel2 = false;
-      show-notifications = true;
-      show-preferences = true;
-      show-quickmenu-subtitle = true;
       show-system-indicator = false;
     };
 
     "Bluetooth-Battery-Meter" = {
-      device-list = [
-        ''
-          {"path":"/org/bluez/hci0/dev_90_DA_07_6C_92_8C","icon":"audio-headset","alias":"Philips TAH5209","paired":true,"battery-reported":true,"qs-level":true,"indicator-mode":2,"enhanced-device":null,"connected-time":1769782832,"disconnected-time":1769782832}
-        ''
-      ];
+      device-list = [ ''
+        {"path":"/org/bluez/hci0/dev_90_DA_07_6C_92_8C","icon":"audio-headset","alias":"Philips TAH5209","paired":true,"battery-reported":true,"qs-level":true,"indicator-mode":2,"enhanced-device":null,"connected-time":1773482945,"disconnected-time":1773482972}
+      '' ];
     };
 
     "blur-my-shell" = {
@@ -260,22 +255,11 @@ with lib.hm.gvariant;
     };
 
     "caffeine" = {
-      duration-timer-list = [
-        900
-        2700
-        5100
-      ];
+      countdown-timer = 2700;
+      duration-timer-list = [ 900 2700 5100 ];
       indicator-position-max = 5;
       use-custom-duration = true;
-    };
-
-    "com/github/hermes83/compiz-windows-effect" = {
-      last-version = 29;
-      preset = "R";
-    };
-
-    "org/gnome/desktop/interface" = {
-      icon-theme = "Papirus-Dark";
+      user-enabled = false;
     };
 
     "dash-to-dock" = {
@@ -284,22 +268,27 @@ with lib.hm.gvariant;
       dash-max-icon-size = 64;
       dock-position = "BOTTOM";
       height-fraction = 0.9;
-      isolate-locations = true;
       preferred-monitor = -2;
       preferred-monitor-by-connector = "eDP-1";
       scroll-action = "switch-workspace";
+      scroll-to-focused-application = true;
       show-trash = false;
     };
 
+    "gsconnect" = {
+      missing-openssl = false;
+      name = "cutie";
+    };
+
     "just-perfection" = {
+      animation = 1;
       background-menu = false;
       controls-manager-spacing-size = 22;
       dash = true;
       double-super-to-appgrid = false;
       osd = true;
-      osd-position = 1;
       panel = true;
-      panel-size = 22;
+      panel-size = 23;
       ripple-box = false;
       search = false;
       startup-status = 0;
@@ -309,25 +298,23 @@ with lib.hm.gvariant;
       window-picker-icon = false;
       window-preview-caption = false;
       window-preview-close-button = true;
-      workspace = true;
+      workspace = false;
       workspace-background-corner-size = 15;
       workspace-popup = false;
       workspaces-in-app-grid = true;
     };
 
+    "libpanel" = {
+      layout = [ [ "quick-settings-audio-panel@rayzeq.github.io/main" ] [ "gnome@main" ] ];
+    };
+
     "tilingshell" = {
       last-version-name-installed = "17.1";
-      layouts-json = "[{\"id\":\"Layout 1\",\"tiles\":[{\"x\":0,\"y\":0,\"width\":0.22,\"height\":0.5,\"groups\":[1,2]},{\"x\":0,\"y\":0.5,\"width\":0.22,\"height\":0.5,\"groups\":[1,2]},{\"x\":0.22,\"y\":0,\"width\":0.56,\"height\":1,\"groups\":[2,3]},{\"x\":0.78,\"y\":0,\"width\":0.22,\"height\":0.5,\"groups\":[3,4]},{\"x\":0.78,\"y\":0.5,\"width\":0.22,\"height\":0.5,\"groups\":[3,4]}]},{\"id\":\"Layout 2\",\"tiles\":[{\"x\":0,\"y\":0,\"width\":0.22,\"height\":1,\"groups\":[1]},{\"x\":0.22,\"y\":0,\"width\":0.56,\"height\":1,\"groups\":[1,2]},{\"x\":0.78,\"y\":0,\"width\":0.22,\"height\":1,\"groups\":[2]}]},{\"id\":\"Layout 3\",\"tiles\":[{\"x\":0,\"y\":0,\"width\":0.33,\"height\":1,\"groups\":[1]},{\"x\":0.33,\"y\":0,\"width\":0.67,\"height\":1,\"groups\":[1]}]},{\"id\":\"Layout 4\",\"tiles\":[{\"x\":0,\"y\":0,\"width\":0.67,\"height\":1,\"groups\":[1]},{\"x\":0.67,\"y\":0,\"width\":0.33,\"height\":1,\"groups\":[1]}]},{\"id\":\"52558\",\"tiles\":[{\"x\":0,\"y\":0,\"width\":1,\"height\":1,\"groups\":[]}]}]";
+      layouts-json = "[{\"id\":\"Layout 1\",\"tiles\":[{\"x\":0,\"y\":0,\"width\":0.22,\"height\":0.5,\"groups\":[1,2]},{\"x\":0,\"y\":0.5,\"width\":0.22,\"height\":0.5,\"groups\":[1,2]},{\"x\":0.22,\"y\":0,\"width\":0.56,\"height\":1,\"groups\":[2,3]},{\"x\":0.78,\"y\":0,\"width\":0.22,\"height\":0.5,\"groups\":[3,4]},{\"x\":0.78,\"y\":0.5,\"width\":0.22,\"height\":0.5,\"groups\":[3,4]}]},{\"id\":\"Layout 2\",\"tiles\":[{\"x\":0,\"y\":0,\"width\":0.22,\"height\":1,\"groups\":[1]},{\"x\":0.22,\"y\":0,\"width\":0.56,\"height\":1,\"groups\":[1,2]},{\"x\":0.78,\"y\":0,\"width\":0.22,\"height\":1,\"groups\":[2]}]},{\"id\":\"Layout 3\",\"tiles\":[{\"x\":0,\"y\":0,\"width\":0.33,\"height\":1,\"groups\":[1]},{\"x\":0.33,\"y\":0,\"width\":0.67,\"height\":1,\"groups\":[1]}]},{\"id\":\"Layout 4\",\"tiles\":[{\"x\":0,\"y\":0,\"width\":0.67,\"height\":1,\"groups\":[1]},{\"x\":0.67,\"y\":0,\"width\":0.33,\"height\":1,\"groups\":[1]}]},{\"id\":\"71915\",\"tiles\":[{\"x\":0,\"y\":0,\"width\":1,\"height\":1,\"groups\":[]}]}]";
       overridden-settings = "{\"org.gnome.mutter.keybindings\":{\"toggle-tiled-right\":\"['<Super>Right']\",\"toggle-tiled-left\":\"['<Super>Left']\"},\"org.gnome.desktop.wm.keybindings\":{\"maximize\":\"['<Super>Up']\",\"unmaximize\":\"['<Super>Down', '<Alt>F5']\"},\"org.gnome.mutter\":{\"edge-tiling\":\"true\"}}";
-      selected-layouts = [
-        [ "Layout 1" ]
-        [ "Layout 1" ]
-        [ "Layout 1" ]
-        [ "Layout 1" ]
-        [ "Layout 1" ]
-        [ "Layout 1" ]
-      ];
+      selected-layouts = [ [ "Layout 1" ] [ "Layout 1" ] [ "Layout 1" ] [ "Layout 1" ] ];
       show-indicator = false;
+      snap-assistant-threshold = 55;
       window-use-custom-border-color = false;
     };
 
@@ -407,6 +394,7 @@ with lib.hm.gvariant;
         "just-perfection-desktop@just-perfection"
         "weatheroclock@clemens.lab21.org"
         "weatheroclock@CleoMenezesJr.github.io"
+        "quick-settings-audio-panel@rayzeq.github.io"
         "quick-settings-tweaker@qwreey"
         "mediacontrols@cliffniff.github.com"
         "wiggle@meghatsh.github.com"
