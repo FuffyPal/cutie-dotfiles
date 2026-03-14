f5fe458570fbdb41b2a71279576a8a5e77d304cd
Fluffy Pal <email@email.com>
2026-03-14 16:42:22 +0300

    awdawf

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 RED='\033[0;31m'
 NC='\033[0m'
-git add -N . 
+git add -N .
 
 echo -e "${BLUE}🔍 Nix Flake yapısı kontrol ediliyor...${NC}"
 
@@ -19,6 +19,14 @@ fi
 echo -e "${BLUE}🛠️  Tüm sistem inşa ediliyor (Dry-run)...${NC}"
 
 if nix build .#nixosConfigurations.cutie.config.system.build.toplevel --dry-run; then
+    echo -e "${GREEN}✅ İnşa başarılı! Hem sistem hem kullanıcı ayarları uyumlu.${NC}"
+else
+    echo -e "${RED}❌ İnşa başarısız! Mantıksal bir çakışma var.${NC}"
+    git reset
+    exit 1
+fi
+
+if nix build .#nixosConfigurations.retrex.config.system.build.toplevel --dry-run; then
     echo -e "${GREEN}✅ İnşa başarılı! Hem sistem hem kullanıcı ayarları uyumlu.${NC}"
 else
     echo -e "${RED}❌ İnşa başarısız! Mantıksal bir çakışma var.${NC}"
