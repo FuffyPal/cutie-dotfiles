453c479a5829c4de5783f57f3121f8cc2796599c
Fluffy Pal <fluffypal17@gmail.com>
2026-03-17 23:19:19 +0300

    feat: add Antigravity RPM repository and install antigravity package.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,5 @@
 sudo zypper dup --no-recommends -y 
 sudo zypper install openSUSE-repos-Leap-NVIDIA -y
 sudo zypper install-new-recommends -y
+sudo zypper addrepo --refresh--check --name "Antigravity RPM Repository" https://us-central1-yum.pkg.dev/projects/antigravity-auto-updater-dev/antigravity-rpm antigravity-rpm
+sudo zypper in -y antigravity 
