63c002c225d334a63197646bfe83f5e70de43dc0
Fluffy Pal <fluffypal17@gmail.com>
2026-03-17 20:07:04 +0300

    chore: add multiple GNOME applications to the debloat list.

--- scripts/debloat.sh
+++ scripts/debloat.sh
@@ -1,9 +1,19 @@
-# Örnek Debloat listesi
 bloatware=(
     "gnome-games"
     "gnome-weather"
     "gnome-maps"
+    "gnome-calculator"
     "evolution"
+    "gnome-characters"
+    "gnome-chess"
+    "gnome-clocks"
+    "gnome-contacts"
+    "gnome-extensions-app"
+    "gnome-mahjongg"
+    "gnome-mines"
+    "gnome-music"
+    "gnome-sudoku"
+    
 )
 
 sudo zypper rm -u "${bloatware[@]}"
