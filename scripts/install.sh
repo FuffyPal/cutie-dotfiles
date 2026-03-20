5f709f926a44b19006b284a634917730a9208940
Fluffy Pal <fluffypal17@gmail.com>
2026-03-21 00:33:01 +0300

    chore: Use the official Nvidia repository for graphics driver installation and reorder the `install-new-recommends` command.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 sudo zypper dup --no-recommends -y 
-sudo zypper install openSUSE-repos-Leap-NVIDIA -y
-sudo zypper install-new-recommends -y
 sudo zypper addrepo --refresh --check --name "Antigravity RPM Repository" https://us-central1-yum.pkg.dev/projects/antigravity-auto-updater-dev/antigravity-rpm antigravity-rpm
 sudo zypper addrepo --refresh --check --name "Packman Repository" -f http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Leap_16.0/ packman
+sudo zypper addrepo --refresh --check --name "Nvidia Repository" -f https://download.nvidia.com/opensuse/leap/16.0 NVIDIA
 sudo zypper in -y antigravity git git-lfs handbrake-cli handbrake-gtk papirus-icon-theme firewalld firewall-config helix lolcat btop systemd-resolved
+sudo zypper install-new-recommends -y
