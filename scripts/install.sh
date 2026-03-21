9a65873e8826bfca9822675e02dfb19f2905c12a
Fluffy Pal <fluffypal17@gmail.com>
2026-03-22 00:54:55 +0300

    feat: add vscoidum repo and package add but remove antigravity package

--- scripts/install.sh
+++ scripts/install.sh
@@ -19,6 +19,17 @@ enabled=1
 gpgcheck=0
 EOL
 
+sudo tee -a /etc/yum.repos.d/vscodium.repo << 'EOF'
+[gitlab.com_paulcarroty_vscodium_repo]
+name=gitlab.com_paulcarroty_vscodium_repo
+baseurl=https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/rpms/
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg
+metadata_expire=1h
+EOF
+
 package="
 podman
 podman-docker
@@ -30,7 +41,7 @@ flatpak
 vim
 gnome-tweaks
 papirus-icon-theme
-antigravity
+codium
 git-lfs
 git
 ptyxis
