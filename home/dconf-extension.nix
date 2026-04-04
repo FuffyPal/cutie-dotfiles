4519a2d0e78876a1b57788a26ee3f213bde6ef9b
Fluffy Pal <fluffypal17@gmail.com>
2026-04-04 16:29:36 +0300

    fwafawf

--- home/dconf-extension.nix
+++ home/dconf-extension.nix
@@ -5,7 +5,7 @@ with lib.hm.gvariant;
 
 {
   dconf.settings = {
-    "/org/gnome/shell/extensions/Battery-Health-Charging" = {
+    "org/gnome/shell/extensions/Battery-Health-Charging" = {
       charging-mode = "max";
       device-type = 12;
       icon-style-type = 0;
@@ -14,16 +14,16 @@ with lib.hm.gvariant;
       show-system-indicator = false;
     };
 
-    "/org/gnome/shell/extensions/blur-my-shell" = {
+    "org/gnome/shell/extensions/blur-my-shell" = {
       settings-version = 2;
     };
 
-    "/org/gnome/shell/extensions/blur-my-shell/appfolder" = {
+    "org/gnome/shell/extensions/blur-my-shell/appfolder" = {
       brightness = 0.6;
       sigma = 30;
     };
 
-    "/org/gnome/shell/extensions/blur-my-shell/dash-to-dock" = {
+    "org/gnome/shell/extensions/blur-my-shell/dash-to-dock" = {
       blur = true;
       brightness = 0.6;
       sigma = 30;
@@ -31,22 +31,22 @@ with lib.hm.gvariant;
       style-dash-to-dock = 0;
     };
 
-    "/org/gnome/shell/extensions/blur-my-shell/panel" = {
+    "org/gnome/shell/extensions/blur-my-shell/panel" = {
       brightness = 0.6;
       sigma = 30;
     };
 
-    "/org/gnome/shell/extensions/blur-my-shell/window-list" = {
+    "org/gnome/shell/extensions/blur-my-shell/window-list" = {
       brightness = 0.6;
       sigma = 30;
     };
 
-    "/org/gnome/shell/extensions/caffeine" = {
+    "org/gnome/shell/extensions/caffeine" = {
       indicator-position-max = 3;
       user-enabled = true;
     };
 
-    "/org/gnome/shell/extensions/dash-to-dock" = {
+    "org/gnome/shell/extensions/dash-to-dock" = {
       background-opacity = 0.8;
       custom-theme-shrink = true;
       dash-max-icon-size = 64;
@@ -58,17 +58,17 @@ with lib.hm.gvariant;
       show-trash = false;
     };
 
-    "/org/gnome/shell/extensions/forge" = {
+    "org/gnome/shell/extensions/forge" = {
       css-last-update = mkUint32 37;
     };
 
-    "/org/gnome/shell/extensions/gsconnect" = {
+    "org/gnome/shell/extensions/gsconnect" = {
       devices = [ "1b2f486cc1ac4c2fab7ef66faef0ad26" ];
       missing-openssl = false;
       name = "cutie";
     };
 
-    "/org/gnome/shell/extensions/just-perfection" = {
+    "org/gnome/shell/extensions/just-perfection" = {
       background-menu = false;
       controls-manager-spacing-size = 22;
       dash = true;
@@ -92,19 +92,19 @@ with lib.hm.gvariant;
       workspaces-in-app-grid = true;
     };
 
-    "/org/gnome/shell/extensions/libpanel" = {
+    "org/gnome/shell/extensions/libpanel" = {
       layout = [ [ "gnome@main" "quick-settings-audio-panel@rayzeq.github.io/main" ] ];
     };
 
-    "/org/gnome/shell/extensions/quick-settings-audio-panel" = {
+    "org/gnome/shell/extensions/quick-settings-audio-panel" = {
       version = 2;
     };
 
-    "/org/gnome/shell/extensions/quick-settings-avatar" = {
+    "org/gnome/shell/extensions/quick-settings-avatar" = {
       avatar-position = 1;
     };
 
-    "/org/gnome/shell/extensions/tilingshell" = {
+    "org/gnome/shell/extensions/tilingshell" = {
       last-version-name-installed = "17.1";
       layouts-json = "[{\"id\":\"Layout 1\",\"tiles\":[{\"x\":0,\"y\":0,\"width\":0.22,\"height\":0.5,\"groups\":[1,2]},{\"x\":0,\"y\":0.5,\"width\":0.22,\"height\":0.5,\"groups\":[1,2]},{\"x\":0.22,\"y\":0,\"width\":0.56,\"height\":1,\"groups\":[2,3]},{\"x\":0.78,\"y\":0,\"width\":0.22,\"height\":0.5,\"groups\":[3,4]},{\"x\":0.78,\"y\":0.5,\"width\":0.22,\"height\":0.5,\"groups\":[3,4]}]},{\"id\":\"Layout 2\",\"tiles\":[{\"x\":0,\"y\":0,\"width\":0.22,\"height\":1,\"groups\":[1]},{\"x\":0.22,\"y\":0,\"width\":0.56,\"height\":1,\"groups\":[1,2]},{\"x\":0.78,\"y\":0,\"width\":0.22,\"height\":1,\"groups\":[2]}]},{\"id\":\"Layout 3\",\"tiles\":[{\"x\":0,\"y\":0,\"width\":0.33,\"height\":1,\"groups\":[1]},{\"x\":0.33,\"y\":0,\"width\":0.67,\"height\":1,\"groups\":[1]}]},{\"id\":\"Layout 4\",\"tiles\":[{\"x\":0,\"y\":0,\"width\":0.67,\"height\":1,\"groups\":[1]},{\"x\":0.67,\"y\":0,\"width\":0.33,\"height\":1,\"groups\":[1]}]}]";
       overridden-settings = "{\"org.gnome.mutter.keybindings\":{\"toggle-tiled-right\":\"['<Super>Right']\",\"toggle-tiled-left\":\"['<Super>Left']\"},\"org.gnome.desktop.wm.keybindings\":{\"maximize\":\"['<Super>Up']\",\"unmaximize\":\"['<Super>Down', '<Alt>F5']\"},\"org.gnome.mutter\":{\"edge-tiling\":\"true\"}}";
