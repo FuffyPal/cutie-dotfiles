de84a6788c667a2519824ed169cec1fdd8977ff6
Flaouve <flaouve@gmail.com>
2026-04-20 13:22:12 +0300

    feat(install.sh): add CUDA repository to installation script

--- install.sh
+++ install.sh
@@ -91,7 +91,9 @@ enabled=1
 gpgcheck=1
 gpgkey=https://pkg.cloudflareclient.com/pubkey.gpg
 EOF
-
+    info "Cude Repo ekleniyor..."
+    sudo dnf config-manager addrepo --from-repofile https://developer.download.nvidia.com/compute/cuda/repos/fedora$(rpm -E %fedora)/x86_64/cuda-fedora$(rpm -E %fedora).repo
+    
     success "Repo'lar hazır."
 }
 
