63945a68518f00676b3a3c647666b2d645fc7dd5
Flaouve <flaouve@gmail.com>
2026-04-20 05:28:58 +0300

    fix: update dns.sh to use the correct resolved.conf path and remove unnecessary directory creation

--- scripts/dns.sh
+++ scripts/dns.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-RESOLVED_CONF_PATH_TO="/etc/systemd/resolved.conf.d"
-RESOLVED_CONF="$RESOLVED_CONF_PATH_TO/fluffy.conf"
-
+RESOLVED_CONF="/etc/systemd/resolved.conf"
 NEW_CONFIG="[Resolve]
 DNS=1.1.1.1#cloudflare-dns.com 
 FallbackDNS=1.0.0.1#cloudflare-dns.com 
@@ -11,8 +9,6 @@ DNSOverTLS=yes
 Cache=yes
 CacheFromLocalhost=no
 ReadEtcHosts=yes"
-
-sudo mkdir -p "$RESOLVED_CONF_PATH_TO"
 
 echo "$NEW_CONFIG" | sudo tee "$RESOLVED_CONF" > /dev/null
 
