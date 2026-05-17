753423d5f1307e96034f5903f111c45f26306508
Flaouve <flaouve@gmail.com>
2026-05-17 07:44:10 +0300

    feat: add ex apply-live

--- install-immutable.sh
+++ install-immutable.sh
@@ -42,7 +42,7 @@ setup_repos() {
     sudo rpm-ostree install \
       https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
       https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-    sudo rpm-ostree override replace
+    sudo rpm-ostree ex apply-live
     success "Repo'lar hazır."
 }
 
