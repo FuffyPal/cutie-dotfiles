dcdd844e434f3b7dae7ee9d614fe9fbb0930bfe1
Flaouve <flaouve@gmail.com>
2026-05-27 21:21:07 +0300

    chore: update server script configuration and environment settings

--- server.sh
+++ server.sh
@@ -65,8 +65,8 @@ compose_setup() {
         if ! command -v git &>/dev/null; then
             error "Git Not Found."
         else
-            mkdir -p "$DOTFILES_DIR/server/docker"
-            cd "$DOTFILES_DIR/server/docker"
+            mkdir -p ~/pal-clouddyy-stack
+            cd ~/pal-clouddyy-stack
             git clone https://gitlab.com/FluffyPal/pal-clouddyy-stack.git
             podman-compose build
             success "Stack updated."
