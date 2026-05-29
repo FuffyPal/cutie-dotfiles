3806bf3eb4d4f9de9068224c560ede9fd255bde6
flaouve <flaouve@gmail.com>
2026-05-29 14:12:30 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -34,5 +34,13 @@ else
     exit 1
 fi
 
+if nix build .#homeConfigurations."flaouve@it".activationPackage --extra-experimental-features "nix-command flakes" --dry-run; then
+    echo -e "${GREEN}✅ flaouve@it: İnşa başarılı! Home Manager dotfile ayarları uyumlu.${NC}"
+else
+    echo -e "${RED}❌ flaouve@it: İnşa başarısız! Home Manager ayarlarında çakışma var.${NC}"
+    git reset
+    exit 1
+fi
+
 echo -e "${BLUE}👋 Test tamamlandı. Her şey kuruluma hazır!${NC}"
 git reset
