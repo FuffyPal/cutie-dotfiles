c435ee10c9606709423cf64b38b3e933f34dbe41
flaouve <flaouve@gmail.com>
2026-06-08 01:03:46 +0300

    feat: add kitty and config

---
+++ home/kitty.nix
@@ -0,0 +1,12 @@
+{
+  programs.kitty = {
+    enable = true;
+    theme = "Catppuccin-Mocha";
+    settings = {
+      font_family = "FiraCode Nerd Font";
+      font_size = "11.0";
+      background_opacity = "0.9";
+      window_padding_width = "8";
+    };
+  };
+}
