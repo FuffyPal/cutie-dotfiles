f7e5e81e69cb85dad29b3460ff8b249ee4ef9594
flaouve <flaouve@gmail.com>
2026-06-08 14:13:26 +0300

    Update fonts.nix

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -1,11 +1,29 @@
 { pkgs, ... }: {
-  fonts.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-cjk-serif
-    noto-fonts-color-emoji
-    nerd-fonts.jetbrains-mono
-    ipafont
-    nanum
-  ];
+  fonts = {
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-color-emoji
+      nerd-fonts.jetbrains-mono
+      ipafont
+      nanum
+    ];
+
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        monospace = [ "JetBrainsMono Nerd Font" ];
+        sansSerif = [
+          "Noto Sans"
+          "JetBrainsMono Nerd Font"
+        ];
+        serif = [
+          "Noto Serif"
+          "JetBrainsMono Nerd Font"
+        ];
+        emoji = [ "Noto Color Emoji" ];
+      };
+    };
+  };
 }
